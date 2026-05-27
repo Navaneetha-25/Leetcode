9aba16a5fa231ddf3302067fd23b9e6d8576e092
Navaneetha-25 <navaneethac2006@gmail.com>
2026-05-27 12:26:12 +0530

    Time: 15 ms (63.31%), Space: 93.5 MB (40.93%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n=nums.length;
+        Set<Integer> set=new HashSet<>();
+        for(int num:nums){
+            if(set.contains(num)) return true;
+            set.add(num);
+        }
+        return false;
+    }
+}
