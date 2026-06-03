d32c5dcd3be51f0c3332ca9688045f76013efd58
Navaneetha-25 <navaneethac2006@gmail.com>
2026-06-03 11:34:18 +0530

    Time: 1 ms (20.81%), Space: 43.6 MB (23.09%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        
+        for(int i=1;i<nums.length;i++){
+            int j=i-1;
+            int key=nums[i];
+
+            while(j>=0 && nums[j]>key){
+                nums[j+1]=nums[j];
+                j=j-1;
+            }
+            nums[j+1]=key;
+        }
+    }
+}
